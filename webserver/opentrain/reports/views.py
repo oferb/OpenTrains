ad3a21696364bb9cbe9ba32de3a472b0bdbe6c82
Eran Keydar <ekeydar@gmail.com>
2013-12-02 15:44:33 +0200

    json

--- webserver/opentrain/reports/views.py
+++ webserver/opentrain/reports/views.py
@@ -1,13 +1,16 @@
 from django.http.response import HttpResponseNotAllowed, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
+import json
+
 # Create your views here.
 
 @csrf_exempt
 def add(req):
     if req.method != 'POST':
         return HttpResponseNotAllowed(permitted_methods=["POST"],content="405 - ONLY POST")
-    print req.body
+    body = req.body
+    print json.dumps(json.loads(body),indent=4)
     
     return HttpResponse(status=201,content="report accepted")
 

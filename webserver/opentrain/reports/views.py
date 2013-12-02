506b32ada490238fc85fccc25f89962a0ac95759
Eran Keydar <ekeydar@gmail.com>
2013-12-02 18:07:13 +0200

    raw reports

--- webserver/opentrain/reports/views.py
+++ webserver/opentrain/reports/views.py
@@ -9,18 +9,15 @@ MAX_REPORTS = 10
 def add(req):
     if req.method != 'POST':
         return HttpResponseNotAllowed(permitted_methods=["POST"],content="405 - ONLY POST")
-    try:
-        body = req.body
-        text = json.dumps(json.loads(body))
-        rep = models.RawReport(text=text)
-        rep.save()
-        count = models.RawReport.objects.all().count()
-        if count > MAX_REPORTS:
-            r = models.RawReport.objects.all().order_by('id')[count-MAX_REPORTS]
-            models.RawReport.objects.filter(id__lt=r.id).delete()
-    except Exception,e:
-        print e
-        raise e
+    
+    body = req.body
+    text = json.dumps(json.loads(body))
+    rep = models.RawReport(text=text)
+    rep.save()
+    #count = models.RawReport.objects.all().count()
+    #if count > MAX_REPORTS:
+    #    r = models.RawReport.objects.all().order_by('id')[count-MAX_REPORTS]
+    #    models.RawReport.objects.filter(id__lt=r.id).delete()
     
     return HttpResponse(status=201,content="report accepted")
 

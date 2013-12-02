bd1e0e56fcda5bf64957e7c468f3c902196c4de9
Eran Keydar <ekeydar@gmail.com>
2013-12-02 15:38:43 +0200

    csrf

--- webserver/opentrain/reports/views.py
+++ webserver/opentrain/reports/views.py
@@ -1,7 +1,9 @@
 from django.http.response import HttpResponseNotAllowed, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
+@csrf_exempt
 def add(req):
     if req.method != 'POST':
         return HttpResponseNotAllowed(permitted_methods=["POST"],content="405 - ONLY POST")

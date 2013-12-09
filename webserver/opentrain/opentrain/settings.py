fb406a046772cf2e2b91203eb1ad986aecef26a4
Eran Keydar <ekeydar@gmail.com>
2013-12-09 15:57:20 +0200

    rearrange

--- webserver/opentrain/opentrain/settings.py
+++ webserver/opentrain/opentrain/settings.py
@@ -94,10 +94,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'all_static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
-
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,'templates')
                  )
 

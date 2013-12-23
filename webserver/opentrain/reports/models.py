c8e732ed62e4d953dd9d4c82eafcc6b9a424102d
Eran Keydar <ekeydar@gmail.com>
2013-12-23 15:28:20 +0200

    fix

--- webserver/opentrain/reports/models.py
+++ webserver/opentrain/reports/models.py
@@ -5,13 +5,15 @@ import json
 
 class RawReport(models.Model):
     text = models.TextField()
-    saved_at = models.DateTimeField(auto_now_add=True)
+    #saved_at = models.DateTimeField(auto_now_add=True)
     def get_text_as_dict(self):
         return json.loads(self.text)
     def get_text_nice(self):
         return json.dumps(json.loads(self.text),indent=4)
     def to_json(self):
-        return dict(text=self.text,saved_at=self.saved_at.isoformat(),id=self.id)
+        return dict(text=self.text,
+                    id=self.id)
+
 
 
     

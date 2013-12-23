05254dc9fa9ce807c2323321d603bfbfc5328ed8
Eran Keydar <ekeydar@gmail.com>
2013-12-23 16:22:53 +0200

    loc info

--- webserver/opentrain/analysis/logic.py
+++ webserver/opentrain/analysis/logic.py
@@ -7,24 +7,41 @@ def analyze_raw_reports(clean=True):
     if clean:
         delete_all_reports()
     items = _collect_all_items()
+    wifis = []
+    locs = []
     for (idx,item) in enumerate(items):
         if idx % 100 == 0:
             print '%d/%d' % (idx,len(items))
         if 'wifi' in item.keys():
-            dt = common.ot_utils.get_utc_time_from_timestamp(float(item['time'])/1000)
-            m = models.WifiReport(device_id=item['device_id'],timestamp=dt)
+            report_dt = common.ot_utils.get_utc_time_from_timestamp(float(item['time'])/1000)
+            m = models.Report(device_id=item['device_id'],timestamp=report_dt)
             m.save()
+            item_loc = item.get('location_api')
+            if item_loc:
+                loc = models.LocationInfo(report=m,
+                                          lat=item_loc['lat'],
+                                          lng=item_loc['long'],
+                                          provider=item_loc['provider'],
+                                          timestamp = common.ot_utils.get_utc_time_from_timestamp(float(item_loc['time'])/1000),
+                                          accuracy = item_loc['accuracy'])
+                locs.append(loc)
             for wifi in item['wifi']:
-                w = models.WifiInReport(SSID=wifi['SSID'],
+                wifis.append(models.SingleWifiReport(SSID=wifi['SSID'],
                                  signal=wifi['signal'],
                                  frequency=wifi['frequency'],
-                                 key=wifi['key'])
-                m.wifi_set.add(w)
+                                 key=wifi['key'],
+                                 report=m))
+    print 'Saving all dependant objects'
+    models.SingleWifiReport.objects.bulk_create(wifis)
+    models.LocationInfo.objects.bulk_create(locs)
+    
+    
 
 
 def delete_all_reports():
-    common.ot_utils.delete_from_model(models.WifiInReport)
-    common.ot_utils.delete_from_model(models.WifiReport)
+    common.ot_utils.delete_from_model(models.SingleWifiReport)
+    common.ot_utils.delete_from_model(models.LocationInfo)
+    common.ot_utils.delete_from_model(models.Report)
     
 def _collect_all_items():
     all_reports = reports.models.RawReport.objects.all()
--- webserver/opentrain/analysis/models.py
+++ webserver/opentrain/analysis/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 
-class WifiReport(models.Model):
+class Report(models.Model):
     device_id = models.CharField(max_length=50)
     timestamp = models.DateTimeField()
     created = models.DateTimeField(auto_now_add=True)
-
-class WifiInReport(models.Model):
-    report = models.ForeignKey(WifiReport,related_name='wifi_set')
+    
+class LocationInfo(models.Model):
+    report = models.ForeignKey(Report,related_name='my_loc')
+    accuracy = models.FloatField()
+    lat = models.FloatField()
+    lng = models.FloatField()
+    provider = models.CharField(max_length=100)
+    timestamp = models.DateTimeField()
+    
+class SingleWifiReport(models.Model):
+    report = models.ForeignKey(Report,related_name='wifi_set')
     SSID = models.CharField(max_length=20)
     frequency = models.FloatField()
     key = models.CharField(max_length=20)

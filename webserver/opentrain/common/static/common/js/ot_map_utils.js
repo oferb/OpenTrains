0574b37ecc710e7d5d5ef3a176ec29dbbf77e7c8
Eran Keydar <ekeydar@gmail.com>
2013-12-30 22:06:03 +0200

    openlayers

--- webserver/opentrain/common/static/common/js/ot_map_utils.js
+++ webserver/opentrain/common/static/common/js/ot_map_utils.js
@@ -1,2 +1,9 @@
 // utility functions for open layers
 
+function getLonLat(lon,lat) {
+	var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
+    var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
+    return new OpenLayers.LonLat(lon,lat).transform( fromProjection, toProjection); 
+}
+
+

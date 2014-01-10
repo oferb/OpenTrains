600c2ea403ba4b83b514c2df3ad717167310d878
Eran Keydar <ekeydar@gmail.com>
2014-01-10 15:49:18 +0200

    fix

--- webserver/opentrain/analysis/views.py
+++ webserver/opentrain/analysis/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import render
 import forms
+from django.views.generic.base import View
+from django.http.response import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
 # Create your views here.
 
 def show_labels(req):
     ctx = dict(form=forms.LabelsForm)
     return render(req,'analysis/show_labels.html',ctx)
 
-def show_reports(req):
-    ctx = dict(form=forms.ReportsForm)
-    return render(req,'analysis/show_reports.html',ctx)
+
+class ShowDeviceReports(View):
+    def get(self,req,*args,**kwargs):
+        ctx = dict(form=forms.ReportsForm())
+        return render(req,'analysis/show_reports.html',ctx) 
+    
+    def post(self,req,*args,**kwargs):
+        import urllib
+        form = forms.ReportsForm(req.POST)
+        if form.is_valid():
+            params=dict(timestamp=form.cleaned_data['timestamp'],
+                        device_id=form.cleaned_data['device_id']
+                        )
+            qs = urllib.urlencode(params)
+            url = reverse('analysis:show-device-reports')
+            return HttpResponseRedirect('%s?%s' % (url,qs))
+        raise Exception('Illegal Form')
+
+def show_reports_on_map(req):
+    return HttpResponse('not yet')        
 
 
 

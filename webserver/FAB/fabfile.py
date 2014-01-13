5950677fca67bd3cd025e32df827999d727ea449
Eran Keydar <ekeydar@gmail.com>
2014-01-13 23:31:01 +0200

    fab

--- webserver/FAB/fabfile.py
+++ webserver/FAB/fabfile.py
@@ -10,7 +10,7 @@ env.user = 'opentrain'
 # Ec2 configuration
 #env.hosts = ['ec2-50-16-67-1.compute-1.amazonaws.com']
 #env.user = 'ubuntu'
-#env.key_filename = os.path.expanduser('~/chat.pem')
+#env.key_filename = os.path.expanduser('~/key.pem')
 
 env.django_base_dir = os.path.join('/home/%s/' % (env.user),'work/OpenTrains/webserver/opentrain')
 env.repo = 'https://github.com/oferb/OpenTrains.git'

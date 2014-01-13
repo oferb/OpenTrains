c9c3de23241bc27ee44e449b80e9dce36f5aac42
Eran Keydar <ekeydar@gmail.com>
2014-01-13 16:34:02 +0200

    backup

--- webserver/opentrain/restore.py
+++ webserver/opentrain/restore.py
@@ -2,8 +2,15 @@
 
 import os
 os.environ['DJANGO_SETTINGS_MODULE']='opentrain.settings'
-import reports.logic
-reports.logic.restore_reports('/tmp/backup.gz')
+
+def main():
+    import reports.logic
+    import analysis.logic
+    reports.logic.restore_reports('/tmp/backup.gz')
+    analysis.logic.analyze_raw_reports()
+
+if __name__ == '__main__':
+    main()
 
 
 

91b2f8910aa7b171a37e2f5175e252dd07bdaad7
Eran Keydar <ekeydar@gmail.com>
2014-01-18 01:32:53 +0200

    fix active menu

--- webserver/opentrain/common/ctx.py
+++ webserver/opentrain/common/ctx.py
@@ -1,11 +1,15 @@
+from django.core.urlresolvers import reverse
 def menu(req):
     menu_items = [#dict(title="Search Between",name = 'gtfs:search-between'),
                   dict(title="Search In",name = 'gtfs:search-in'),
-                  dict(title="Show labels",name = 'analysis:show-labels'),
+                  #dict(title="Show labels",name = 'analysis:show-labels'),
                   dict(title="Show Reports",name = 'analysis:select-device-reports')
                   ]
     for mi in menu_items:
-        mi['active'] = False
+        if req.path == reverse(mi['name']):
+            mi['active'] = True
+        else:
+            mi['active'] = False
     return dict(menu_items = menu_items)
 
 

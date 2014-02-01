273b48f2da1e5dd925f1fb1607437c426ffc5635
Eran Keydar <ekeydar@gmail.com>
2014-02-01 18:58:45 +0200

    work

---
+++ webserver/opentrain/analysis/static/analysis/js/app/live_trains.js
@@ -0,0 +1,8 @@
+// app.js
+
+app = angular.module('live_trains', ['my.services', 'my.filters', 'my.directives', 'my.leaflet', 'leaflet-directive']);
+
+app.controller('LiveTrainsController', ['$scope', 'MyHttp', 'MyUtils', 'MyLeaflet', '$timeout', 'leafletData', '$window', '$interval',
+function($scope, MyHttp, MyUtils, MyLeaflet, $timeout, leafletData, $window, $interval) {
+}]);
+

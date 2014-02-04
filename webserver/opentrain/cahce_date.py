23e0bd456b7208a300e74b2bf72d1450c5eef8c3
Eran Keydar <ekeydar@gmail.com>
2014-02-04 14:06:07 +0200

    cache

--- webserver/opentrain/cahce_date.py
+++ webserver/opentrain/cahce_date.py
@@ -5,7 +5,8 @@ import argparse
 
 def cache_date(year,month,day,server):
     print 'Caching for %s/%s/%s on %s' % (day,month,year,server)
-
+    trip_ids = requests.get('http://%s/gtfs/trips-for-date' % (server),params=dict(year=year,month=month,day=day)).json()
+    print 'Found %s trip ids for %s/%s/%s' % (len(trip_ids),day,month,year)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='cache trips for date')

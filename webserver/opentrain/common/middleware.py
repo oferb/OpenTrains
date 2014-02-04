f71370e72b47bbb539259e77e1b93f3deee3e5c6
Eran Keydar <ekeydar@gmail.com>
2014-02-04 16:54:29 +0200

    pref

--- webserver/opentrain/common/middleware.py
+++ webserver/opentrain/common/middleware.py
@@ -1,11 +1,18 @@
 from django.middleware.common import CommonMiddleware
 import urlparse
+import time
 class OpenTrainMiddleware(CommonMiddleware):
     def process_response(self, request, response):
         from django.db import connection
-        print '%s %s db DB = %d' % (request.method,urlparse.unquote(request.get_full_path()),len(connection.queries)) 
+        request.prof_end_time = time.time()
+        total_time = request.prof_end_time - request.prof_start_time
+        print '%s %s Time = %.2f DB = %d' % (request.method,
+                                             urlparse.unquote(request.get_full_path()),
+                                             total_time,
+                                             len(connection.queries)) 
         return response
     
-    
+    def process_request(self,request):
+        request.prof_start_time = time.time()
     
     

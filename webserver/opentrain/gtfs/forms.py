75c2eb81148a472d3cea8890d0a780f63de99678
Eran Keydar <ekeydar@gmail.com>
2014-02-24 16:56:35 +0200

    trans

--- webserver/opentrain/gtfs/forms.py
+++ webserver/opentrain/gtfs/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from bootstrap3_datetime.widgets import DateTimePicker
-
+from django.utils.translation import ugettext as _
 class SearchBetweenForm(forms.Form):
     import logic
     from_station = forms.ChoiceField(choices=logic.get_stations_choices())
@@ -16,10 +16,11 @@ class SearchBetweenForm(forms.Form):
     
 class SearchInForm(forms.Form):
     import logic
-    in_station = forms.ChoiceField(choices=logic.get_stations_choices())
-    before = forms.IntegerField()
-    after = forms.IntegerField()
+    in_station = forms.ChoiceField(choices=logic.get_stations_choices(),label=_("In Station"))
+    before = forms.IntegerField(label=_("Before"))
+    after = forms.IntegerField(label=_("After"))
     when = forms.DateTimeField(
+        label=_("When"),
         input_formats = ['%Y-%m-%d %H:%M'],
         widget=DateTimePicker(options={"format": "YYYY-MM-DD HH:mm",
                                        "pickTime": True,

8ecf18ce3536e0cdd8f365546c8abf9aa21f5cc7
Eran Keydar <ekeydar@gmail.com>
2014-03-03 15:24:50 +0200

    docs

--- webserver/opentrain/ot_api/urls.py
+++ webserver/opentrain/ot_api/urls.py
@@ -1,15 +1,13 @@
 from django.conf.urls import patterns, url
 import views
+print views.ApiView.get_urls()
 
 urlpatterns = patterns('',
-    url(r'^trips/trips-for-date/$',views.get_trip_ids_for_date),
-    url(r'^trips/current/$',views.get_current_trips),
-    url(r'^trips/cur-location/$',views.get_trips_location,name='api-live-trips'),
-    url(r'^trips/(?P<trip_id>\w+)/details/$',views.get_trip_details),
-    url(r'^devices/(?P<device_id>\w+)/reports/',views.get_device_reports), 
-    url(r'^devices/$',views.get_devices),
+                       url('docs',views.show_docs,name="show_docs"),)
+
+urlpatterns += views.ApiView.get_urls() 
+
     
-)
 
 
 
